3476f8bf9f8c64161f20f09fbaeeacd15d33e287
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 19:15:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(number)
   if number < 2 
     return false
   end
-  (2..number).each do |num|
-    if (number % num) === 0 
+  (2...number).each do |num|
+    if (number % num) == 0 
      return false
    end
  end
